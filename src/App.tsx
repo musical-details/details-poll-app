02f9c0e840155d274b7bbf6c6c3d6b8ee17676eb
daddyaddy <at.adriano.at@gmail.com>
2020-03-07 14:58:29 +0100

    [FEAT] Edit App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./assets/details-main-logo.svg";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background"></div>
+      <div className="App-header">
+        <img className="logo" src={logo} />
+        <div>
+          new <strong>alternative</strong> in music world...
+        </div>
+        <div>
+          <button>create it with us</button>
+        </div>
+      </div>
     </div>
   );
 }
