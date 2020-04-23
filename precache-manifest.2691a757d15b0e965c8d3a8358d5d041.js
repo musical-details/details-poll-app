ddd64f77644d08829c59103e371084832c05d866
blossomnight <50330117+blossomnight@users.noreply.github.com>
2020-04-23 22:45:47 +0200

    Updates

---
+++ precache-manifest.2691a757d15b0e965c8d3a8358d5d041.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e222b921361f5cc22809ff042e520dcf",
+    "url": "/details-poll-app/index.html"
+  },
+  {
+    "revision": "940f1d2f4e7185807534",
+    "url": "/details-poll-app/static/css/main.f692fb95.chunk.css"
+  },
+  {
+    "revision": "56f37546c078db9b2570",
+    "url": "/details-poll-app/static/js/2.aa37c61b.chunk.js"
+  },
+  {
+    "revision": "a857e8767947ecbbc4f9bc12f988cfec",
+    "url": "/details-poll-app/static/js/2.aa37c61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940f1d2f4e7185807534",
+    "url": "/details-poll-app/static/js/main.6f374808.chunk.js"
+  },
+  {
+    "revision": "bc115273966b8265bc76",
+    "url": "/details-poll-app/static/js/runtime-main.e84144b1.js"
+  },
+  {
+    "revision": "73dcd75ba878dbaeda19b1dcd7e71d78",
+    "url": "/details-poll-app/static/media/details-main-logo.73dcd75b.svg"
+  },
+  {
+    "revision": "34b4957aa8b0a47620d3ce92f6e90b89",
+    "url": "/details-poll-app/static/media/drums.34b4957a.png"
+  },
+  {
+    "revision": "29b36054487a4da2cc93b4aa9f41f173",
+    "url": "/details-poll-app/static/media/heaphones-icon.29b36054.svg"
+  },
+  {
+    "revision": "2dadaa11f08ee043591d95f601ed8991",
+    "url": "/details-poll-app/static/media/logo-details-search-gradient.2dadaa11.png"
+  },
+  {
+    "revision": "6016e6125842260a32f9fdf6554b00c0",
+    "url": "/details-poll-app/static/media/phone-mock-7.6016e612.png"
+  },
+  {
+    "revision": "a4d405c2e7ee0d95ff692933e54611c0",
+    "url": "/details-poll-app/static/media/rzezba-bg.a4d405c2.png"
+  }
+]);
